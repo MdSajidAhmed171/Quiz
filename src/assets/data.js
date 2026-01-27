fd729c7719ff6194f58f962cc410d44ee69a42c5
Md Sajid Ahmed <md.sajid.ahmed201@gmail.com>
2026-01-27 05:46:26 -0500

    Setup Quiz question

---
+++ src/assets/data.js
@@ -0,0 +1,18 @@
+export const data = [
+    {
+        question: "Whats the capital of the USA?",
+        Option1: "New York",
+        Option2: "New Jersy",
+        Option3: "Florida",
+        Option4: "Washington DC",
+        ans: 1,
+    },
+    {
+        question: "Famous for Aligator?",
+        Option1: "New York",
+        Option2: "New Jersy",
+        Option3: "Florida",
+        Option4: "Washington DC",
+        ans: 3,
+    }
+]
